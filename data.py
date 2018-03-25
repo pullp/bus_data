74bc7e95414c619f8587e458f9f0babc073e815d
Pullp <weixiankui1997@gmail.com>
2018-03-25 20:51:20 +0800

    make sure arrival_time of each trip is ascending

--- data.py
+++ data.py
@@ -42,7 +42,7 @@ base_time = datetime.datetime(1900, 1, 1)
 #
 final['arrival_time'] = final['arrival_time'].apply(lambda time: (datetime.datetime(1900, 1, 1, int(time[:2])%24, int(time[3:5]), int(time[6:])) - base_time).total_seconds())
 #
-v2 = pandas.pivot_table(final, values = [ "arrival_time", "stop_lon",  "stop_lat", ], index =["trip_id", "route_id", "stop_id"])
+v2 = pandas.pivot_table(final, values = [ "stop_lon","stop_id",  "stop_lat" ], index =["trip_id", "route_id",  "arrival_time"])
 #, 
 test = v2[0:100]
 
@@ -52,6 +52,13 @@ current_route_id = ''
 f = open('out.csv', 'w')
 buf = ""
 stop_count = 0
+times = [1, 2]
+def check(times):
+    current_time = times[0]
+    for time in times:
+        if(time<current_time):
+            return 0
+    return 1
 
 for i, row in v2.iterrows():
     if(current_trip_id != i[0]) :
@@ -61,13 +68,14 @@ for i, row in v2.iterrows():
         f.write('\n')
         current_route_id = str(i[1])
         current_trip_id = i[0]
-#        print(type(i[1]))
-#        print(stop_count)
-#        f.write(str(i[1]) +',' + str(stop_count))
-#        f.write('\n')
         stop_count = 0
         buf = ''
-    buf += (str(i[2])+","+str(row['stop_lat'])+"," +str(row['stop_lon']) + "," + str(row['arrival_time']) + '\n')
+        if(check(times) == 0):
+            print(i, row, '\n')
+            print("check error\n")
+            break;
+        times = [1, 2]
+    buf += (str(row['stop_id'])+","+str(row['stop_lat'])+"," +str(row['stop_lon']) + "," + str(i[2]) + '\n')
     stop_count += 1
     
 f.close()
