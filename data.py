ade04631f9e4ee14a645282921e62f798d7ad662
Pullp <weixiankui1997@gmail.com>
2018-03-26 12:21:23 +0800

    use stop_name instead of stop_id in out.csv

--- data.py
+++ data.py
@@ -9,13 +9,13 @@ trips = pandas.read_csv("trips.txt")
 stop_times = pandas.read_csv("stop_times.txt")
 stops = pandas.read_csv("stops.txt")
 
-stop_position = stops.loc[:,['stop_id', 'stop_lon', 'stop_lat']]
+stop_position_name = stops.loc[:,['stop_id', 'stop_lon', 'stop_lat', 'stop_name']]
 route_trip = trips.loc[:,['route_id', 'trip_id']]
 trip_stop_time = stop_times.loc[:, ['trip_id', 'arrival_time', 'stop_id']]
 
 route_trip_stop_time = pandas.merge(route_trip, trip_stop_time, on = "trip_id")
 
-final = pandas.merge(route_trip_stop_time, stop_position, on = "stop_id")
+final = pandas.merge(route_trip_stop_time, stop_position_name, on = "stop_id")
 
 
 base_time = datetime.datetime(1900, 1, 1)
@@ -23,7 +23,7 @@ base_time = datetime.datetime(1900, 1, 1)
 
 final['arrival_time'] = final['arrival_time'].apply(lambda time: (datetime.datetime(1900, 1, 1, int(time[:2])%24, int(time[3:5]), int(time[6:])) - base_time).total_seconds())
 
-v2 = pandas.pivot_table(final, values = [ "stop_lon","stop_id",  "stop_lat" ], index =["trip_id", "route_id",  "arrival_time"])
+v2 = pandas.pivot_table(final, values = [ "stop_lon", "stop_id",  "stop_lat" ], index =["trip_id", "route_id",  "arrival_time", "stop_name"])
 test = v2[0:100]
 
 current_trip_id=''
@@ -55,7 +55,7 @@ for i, row in v2.iterrows():
             print("check error\n")
             break;
         times = [1, 2]
-    buf += (str(row['stop_id'])+","+str(row['stop_lat'])+"," +str(row['stop_lon']) + "," + str(i[2]) + '\n')
+    buf += (str(i[3])+","+str(row['stop_lat'])+"," +str(row['stop_lon']) + "," + str(i[2]) + '\n')
     stop_count += 1
     
 f.close()
